aaeaf2c4e7d5cb61ec437eab22195dbf3e61ecd1
michaelbredman <mredman@gmail.com>
2015-06-16 10:54:09 -0700

    testing jenkins configuration III

--- index.php
+++ index.php
@@ -20,7 +20,6 @@
 </head>
 
 <body>
-<!--This is to test the Jenkins Configuration more and more-->
 <?php include 'gtm_inc.php'; ?>
 <div class="navbar navbar-fixed-top navbar-inverse">
     <div class="navbar-inner">
